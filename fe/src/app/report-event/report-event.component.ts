9e57551759b6e8908d1185e76a1ae55278aa0cbf
CarlaOM <30482992+CarlaOM@users.noreply.github.com>
2018-04-05 12:10:52 -0400

    Reportes

--- fe/src/app/report-event/report-event.component.ts
+++ fe/src/app/report-event/report-event.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { PeticionesService } from '../services/peticiones.service';
 
 @Component({
   selector: 'app-report-event',
   templateUrl: './report-event.component.html',
-  styleUrls: ['./report-event.component.css']
+  styleUrls: ['./report-event.component.css'],
+  providers: [, PeticionesService]
 })
 export class ReportEventComponent implements OnInit {
-
-  constructor() { }
+  public eventId;
+  public event;
+  public inscriptions;
+ 
+  constructor(
+    private _peticionesService: PeticionesService
+  ) { }
 
   ngOnInit() {
+    this._peticionesService.getEvent(this.eventId).subscribe(
+      result => {
+         this.event = result;
+         console.log(this.event)
+         //this.event.map(event => {
+        this.inscriptions = this.event.inscriptions.filter(e => {console.log(e.state == 1); return e.state == 1});
+       //});
+       console.log(this.event)
+         this.inscriptions = this.event.inscritos;
+      },
+      error => {
+         console.log(<any>error)
+      }
+   );
   }
 
 }
