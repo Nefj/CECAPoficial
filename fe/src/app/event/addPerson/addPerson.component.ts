55146dfeb70e317ee5e4b3a036f3584d63f2e7ef
Noel Franco <nefj@outlook.com>
2018-04-10 12:29:06 -0400

    EventTodos

--- fe/src/app/event/addPerson/addPerson.component.ts
+++ fe/src/app/event/addPerson/addPerson.component.ts
@@ -16,9 +16,9 @@ export class AddPersonComponent implements OnInit {
    @ViewChild('ci') ciRef: ElementRef;
    @ViewChild('cellphone') cellphoneRef: ElementRef;
    @ViewChild('email') emailRef: ElementRef;
-//    @ViewChild('ocupation') ocupationRef: ElementRef;
-//    @ViewChild('program') programRef: ElementRef;
-//    @ViewChild('interes') interesRef: ElementRef;
+   //    @ViewChild('ocupation') ocupationRef: ElementRef;
+   //    @ViewChild('program') programRef: ElementRef;
+   //    @ViewChild('interes') interesRef: ElementRef;
    @ViewChild('description') descRef: ElementRef;
    @ViewChild("close", { read: ElementRef }) close: ElementRef;
    @Output() messageEvent = new EventEmitter();
@@ -35,47 +35,47 @@ export class AddPersonComponent implements OnInit {
    public ocupSeleccionado;
    public identy;
    public cartera;
-   constructor(private _peticionesService: PeticionesService) { 
-       this.person = new Person('','',null, null,'',null,'');
-       this.inscription = new Inscription('', '', '', '');
-       //this.identy=Identity._id;
+   constructor(private _peticionesService: PeticionesService) {
+      this.person = new Person('', '', null, null, '', null, '');
+      this.inscription = new Inscription('', '', '', '');
+      //this.identy=Identity._id;
    }
 
    ngOnInit() {
-       this.queryPrograms();
-       this.queryCartera();
-       console.log(this.cartera);
+      // this.queryPrograms();
+      this.queryCartera();
+      console.log(this.cartera);
       // this.interes=[{inscrito:0,confirmado:1,interesado:2,En_duda:3,No_participa:4,Proximo:5}];
-       this.interes=['inscrito','confirmado','interesado','En duda','No participa','Proximo'];
-       console.log(Identity._id+'vamos que se puede')
-    }
-    captProgram(){console.log(
-        this.progSeleccionado);
-        // this.queryIdProgram();
-        // console.log(this.idProgram+ 'hola max');
-    }
-    captInteres(){
-        if(this.intSeleccionado == 'inscrito'){
-            this.interesNum = 0
-        }else{if(this.intSeleccionado == 'confirmado'){
-            this.interesNum = 1
-            }else{if(this.intSeleccionado == 'interesado'){
-                    this.interesNum = 2
-                }else{if(this.intSeleccionado == 'En duda'){
-                        this.interesNum = 3
-                    }else{if(this.intSeleccionado == 'No participa'){
-                            this.interesNum = 4
-                        }else{if(this.intSeleccionado == 'Proximo'){
-                                this.interesNum = 5}
-                        }
-                    }
-                }
-            }
-        }
-        console.log(this.intSeleccionado);
-        console.log(this.interesNum);
-    }
-    captOcupation(){console.log(this.ocupSeleccionado);}
+      this.interes = ['inscrito', 'confirmado', 'interesado', 'En duda', 'No participa', 'Proximo'];
+      console.log(Identity._id + 'vamos que se puede')
+   }
+   //  captProgram(){console.log(
+   //      this.progSeleccionado);
+   //      // this.queryIdProgram();
+   //      // console.log(this.idProgram+ 'hola max');
+   //  }
+   //  captInteres(){
+   //      if(this.intSeleccionado == 'inscrito'){
+   //          this.interesNum = 0
+   //      }else{if(this.intSeleccionado == 'confirmado'){
+   //          this.interesNum = 1
+   //          }else{if(this.intSeleccionado == 'interesado'){
+   //                  this.interesNum = 2
+   //              }else{if(this.intSeleccionado == 'En duda'){
+   //                      this.interesNum = 3
+   //                  }else{if(this.intSeleccionado == 'No participa'){
+   //                          this.interesNum = 4
+   //                      }else{if(this.intSeleccionado == 'Proximo'){
+   //                              this.interesNum = 5}
+   //                      }
+   //                  }
+   //              }
+   //          }
+   //      }
+   //      console.log(this.intSeleccionado);
+   //      console.log(this.interesNum);
+   //  }
+   captOcupation() { console.log(this.ocupSeleccionado); }
 
    save() {
       const firstName = this.firstNameRef.nativeElement.value;
@@ -84,56 +84,56 @@ export class AddPersonComponent implements OnInit {
       //const user = Identity._id;
       const cellphone = this.cellphoneRef.nativeElement.value;
       const email = this.emailRef.nativeElement.value;
-      let cartera='';
-     // if (this.birthdayRef.nativeElement.value == '') birthday = new Date(1, 2, 3);
-    //else birthday = this.birthdayRef.nativeElement.value;
-      const newPerson = new Person(firstName, lastName, ci,cellphone, email, this.ocupSeleccionado, this.cartera._id);
+      // let cartera = '';
+      // if (this.birthdayRef.nativeElement.value == '') birthday = new Date(1, 2, 3);
+      //else birthday = this.birthdayRef.nativeElement.value;
+      const newPerson = new Person(firstName, lastName, ci, cellphone, email, this.ocupSeleccionado, this.cartera._id);
       console.log(newPerson);
       this._peticionesService.addPerson(newPerson).subscribe(response => {
          // console.log(response);
          this.messageEvent.emit();
          this.close.nativeElement.click();
       });
-      
+
    }
 
-   queryPrograms(){
-    this._peticionesService.getPrograms().subscribe(
-        result => {
-           this.programs = result;
-        //    console.log('aqui los programas');
-        //    console.log(this.programs);
-        },
-        error => {
-           var errorMessage = <any>error;
-           console.log(errorMessage);
-        }
-     );
-   }
-//    queryIdProgram(){
-//     this._peticionesService.getIdProgram(this.progSeleccionado).subscribe(
-//         result => {
-//          this.idProgram = result;
-//         //    console.log('ID PROGRAM');
-//         },
-//         error => {
-//            var errorMessage = <any>error;
-//            console.log(errorMessage);
-//         }
-//      );
-//    }
-   queryCartera(){
-       //console.log(Identity._id)
-       this._peticionesService.getUserCartera(Identity._id).subscribe(
-        result => {
-           this.cartera = result;
-           console.log('aqui la cartera del usuario::::');
-           console.log(this.cartera);
-        },
-        error => {
-           var errorMessage = <any>error;
-           console.log(errorMessage);
-        }
-     );
+   // queryPrograms() {
+   //    this._peticionesService.getPrograms().subscribe(
+   //       result => {
+   //          this.programs = result;
+   //          //    console.log('aqui los programas');
+   //          //    console.log(this.programs);
+   //       },
+   //       error => {
+   //          var errorMessage = <any>error;
+   //          console.log(errorMessage);
+   //       }
+   //    );
+   // }
+   //    queryIdProgram(){
+   //     this._peticionesService.getIdProgram(this.progSeleccionado).subscribe(
+   //         result => {
+   //          this.idProgram = result;
+   //         //    console.log('ID PROGRAM');
+   //         },
+   //         error => {
+   //            var errorMessage = <any>error;
+   //            console.log(errorMessage);
+   //         }
+   //      );
+   //    }
+   queryCartera() {
+      //console.log(Identity._id)
+      this._peticionesService.getUserCartera(Identity._id).subscribe(
+         result => {
+            this.cartera = result;
+            console.log('aqui la cartera del usuario::::');
+            console.log(this.cartera);
+         },
+         error => {
+            var errorMessage = <any>error;
+            console.log(errorMessage);
+         }
+      );
    }
 }
