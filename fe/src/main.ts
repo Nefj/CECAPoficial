269f46335bdf65594423af9e6519e4684c987827
alvarodaniel294 <alvarodaniel294@gmail.com>
2018-04-16 18:05:48 -0400

    buscador personas

--- fe/src/main.ts
+++ fe/src/main.ts
@@ -4,6 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './app/event/persona/filter.pipe';
+// import { AppModule } from './app';
+ 
+// platformBrowserDynamic().bootstrapModule(AppModule)
+
 if (environment.production) {
   enableProdMode();
 }
